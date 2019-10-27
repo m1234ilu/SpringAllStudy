787d291c19f8160f0b7f1d4e7ee122665ea26538
crowd <1668591856@qq.com>
2019-10-27 23:42:40 +0800

    提交springBoot_config03

--- Spring_Boot_MyBatis_03/src/main/java/com/springboot/controller/TestController.java
+++ Spring_Boot_MyBatis_03/src/main/java/com/springboot/controller/TestController.java
@@ -1,21 +1,77 @@
 package com.springboot.controller;
 
+import com.springboot.dao.StudentDao;
+import com.springboot.dao.StudentDao2;
 import com.springboot.entity.Student;
-import com.springboot.service.StudentService;
+import com.springboot.entity.Student2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.xml.bind.SchemaOutputResolver;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @RestController
 public class TestController {
 
     @Autowired
-    private StudentService studentService;
+    private StudentDao studentDao;
 
-    @GetMapping(value = "/querystudent")
-    public Student queryStudentBySno(String sno) {
-        return this.studentService.queryStudentBySno(sno);
+    @Autowired
+    private StudentDao2 studentDao2;
+
+    @GetMapping(value = "/queryAllStudent")
+    public List<Student> queryAllStudent() {
+        return this.studentDao.getAllStudent();
     }
+
+    @GetMapping(value = "/selectById/{sno}")
+    public Student selectById(@PathVariable String sno) {
+        return studentDao.selectById(sno);
+    }
+
+    @GetMapping(value = "/selectByUpdateTime/{updateTime}")
+    public List<Student> selectByUpdateTime(@PathVariable String updateTime) {
+        return studentDao.selectByUpdateTime(updateTime);
+    }
+
+    @PostMapping("/insert")
+    public Integer insert(Student student) {
+        //  student.setCreateTime(new java.sql.Date(System.currentTimeMillis()));
+
+        return studentDao.insertSelective(student);
+    }
+
+    @GetMapping(value = "/bigger/{start}")
+    public List<Student> bigger(@PathVariable java.sql.Date start) {
+        return studentDao.bigger(start);
+    }
+
+    @PostMapping("/insert2")
+    public Integer insert2(Student student, String ss) {
+        Student2 student2 = new Student2();
+        student2.setEndtime(student.getUpdateTime());
+        student2.setStarttime(student.getCreateTime());
+        student2.setSs(ss);
+        return studentDao2.insert2(student2);
+    }
+
+    @GetMapping("/insertBatch")
+    public void insertBatch() {
+        List<Student2> list = new ArrayList<>();
+        Student2 student2 = new Student2(new Date(), new Date(), "1");
+        Student2 student21 = new Student2(new Date(), new Date(), "2");
+        list.add(student2);
+        list.add(student21);
+        studentDao2.insertBatch(list);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(new Date());
+    }
+
+
 }
--- Spring_Boot_MyBatis_03/src/main/java/com/springboot/entity/Student.java
+++ Spring_Boot_MyBatis_03/src/main/java/com/springboot/entity/Student.java
@@ -1,6 +1,11 @@
 package com.springboot.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  * @author zycstart
@@ -8,24 +13,65 @@ import java.io.Serializable;
  */
 public class Student implements Serializable {
     private String sno;
-    private String name;
-    private String sex;
+    private String sname;
+    private String ssex;
+    //前台传给后台 时间格式化
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    //设置时区为上海时区，时间格式自己据需求定。
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date updateTime;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd",timezone = "GMT+8")
+    private Date createTime;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd",timezone = "GMT+8")
+    private Date start;
+
     public String getSno() {
         return sno;
     }
+
     public void setSno(String sno) {
         this.sno = sno;
     }
-    public String getName() {
-        return name;
+
+    public String getSname() {
+        return sname;
     }
-    public void setName(String name) {
-        this.name = name;
+
+    public void setSname(String sname) {
+        this.sname = sname;
     }
-    public String getSex() {
-        return sex;
+
+    public String getSsex() {
+        return ssex;
     }
-    public void setSex(String sex) {
-        this.sex = sex;
+
+    public void setSsex(String ssex) {
+        this.ssex = ssex;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
     }
 }
